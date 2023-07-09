de8c2e3adf044ac3e5ba9b2be55563b317e2bdc2
kjdeepak <kjdeepak4work@gmail.com>
2023-07-10 01:23:49 +0530

    Implement swagger documentation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
+});
